6822a1004d99a4d1ccdaec673f35d2b1b02143c5
mgmods786 <82916396+mgmods786@users.noreply.github.com>
2025-01-27 16:17:37 +0800

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -11,11 +11,13 @@ logging.basicConfig(
 
 class Config(object):
     
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7711740007:AAE8n7BHOC2ompDgw0RdIA15dG_OLXUTrA8")
     
-    API_ID = int(os.environ.get("API_ID", ""))
+    API_ID = int(os.environ.get("API_ID", "22216618
+"))
     
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "cb9b9fd32dde8a17b8e55b8131b08ae5
+")
     
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     
@@ -44,19 +46,17 @@ class Config(object):
     
     DEF_WATER_MARK_FILE = "UploadLinkToFileBot"
     
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://mgmods786:<db_@Teenwolf786>@cluster0.0owje.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     
     SESSION_NAME = os.environ.get("SESSION_NAME", "UploadLinkToFileBot")
     
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002294720790"))
     
     LOGGER = logging
-
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
     
-    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+    OWNER_ID = int(os.environ.get("OWNER_ID", "7711740007"))
     
     TG_MIN_FILE_SIZE = 2097152000
     
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "> "@file2linkbg_bot")
                                   
